9266705ddee0a5461e85aebe8bf84802da1d4207
Don <donventura29@gmail.com>
2019-10-22 15:53:46 -0700

    Updating single post and blog page to use correct image sizing

--- template-parts/content-page-title-single-post.php
+++ template-parts/content-page-title-single-post.php
@@ -2,7 +2,7 @@
   //vars
   $page_title = get_field('page_title_content');
 ?>
-<section id="inner-page-title-header" class="d-flex align-items-center page-title-bg page-title" style="position:relative; background-image:url('<?php
+<section id="inner-page-title-header" class="d-flex align-items-center page-title-bg page-title py-80" style="position:relative; background-image:url('<?php
   if ( has_post_thumbnail() ) {
       echo the_post_thumbnail_url();
   }
@@ -17,19 +17,7 @@
 
             <h1 class="mt-0"><?php the_title(); ?></h1>
             <p class="mt-0 mb-0"></p>
-            <div class="blog-photo-meta">
-                <?php 
-                  function my_display_gravatar() { 
-                    // Get User Email Address
-                    $getuseremail = get_the_author_meta('user_email');
-              
-                    // Convert email into md5 hash and set image size to 200 px
-                    $usergravatar = 'https://www.gravatar.com/avatar/' . md5($getuseremail) . '?s=90';
-                    echo '<img src="' . $usergravatar . '" class="wpb_gravatar blog-gravatar" />';
-                  } 
-                  echo my_display_gravatar();
-                ?>
-              </div>
+
 
               <div class="blog-text-meta">
 
